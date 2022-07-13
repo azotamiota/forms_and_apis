d847b7fb9ddaedba64b955aa0f0a219015f9cd1d
azotamiota <azotamiota@gmail.com>
2022-07-13 14:34:30 +0100

    simple DOM element creation

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,15 @@
-console.log("Loaded!")
+const addListItem = (text) => {
+    const container = document.getElementById('container');
+
+    const newLi = document.createElement('li')
+
+    newLi.textContent = text;
+
+    container.appendChild(newLi);
+} 
+
+document.getElementById('first-btn').addEventListener('click', () => {
+    const text = document.querySelector('#input').value
+    addListItem(text)
+})
+
